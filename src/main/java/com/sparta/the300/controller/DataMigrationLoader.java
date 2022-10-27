a63746b4bbfeaa920579c038250428bbe8ce32e7
JamesKempadoo <44989874+JamesKempadoo@users.noreply.github.com>
2022-10-27 13:48:31 +0100

    Removed Non used code

--- src/main/java/com/sparta/the300/controller/DataMigrationLoader.java
+++ src/main/java/com/sparta/the300/controller/DataMigrationLoader.java
@@ -32,36 +32,6 @@ public class DataMigrationLoader {
         }
     }
 
-    public static void concurrently(HashSet<Employee> validEntries, EmployeeDAO employeeDAO, int numberOfThreads) {
-        Thread[] threads = new Thread[numberOfThreads + 1];
-        ArrayList<Employee> arrayList = new ArrayList<>(validEntries);
-        int i = 0;
-        int threadCounter = 0;
-        int x1 = arrayList.size() / numberOfThreads;
-
-        for (i = 0; i <= arrayList.size(); i += arrayList.size() / numberOfThreads) {
-            List<Employee> subArray;
-            if (i + x1 > arrayList.size()) {
-                subArray = arrayList.subList(i, arrayList.size());
-
-            } else {
-                subArray = arrayList.subList(i, i + x1);
-            }
-            //employeeDAO.tryCon();
-            threads[threadCounter] = new Thread(new BatchWorker(subArray, employeeDAO));
-            threads[threadCounter].setName("Thread: " + threadCounter);
-            threads[threadCounter].start();
-            threadCounter++;
-        }
-        for (int j = 0; j < threadCounter; j++) {
-            try {
-                threads[j].join();
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-        }
-    }
-
     private static void retrieval(EmployeeDAO employeeDAO) {
         DisplayManager.printRecordRetrievalMenu();
         try {
@@ -75,18 +45,4 @@ public class DataMigrationLoader {
 
         }
     }
-
-//    private static EmployeeDAO creation() {
-//        long start = System.nanoTime();
-//        HashSet<Employee> validEntries = CSVReader.readDataFile("src/main/resources/EmployeeRecordsLarge.csv", 32);
-//        EmployeeDAO employeeDAO = new EmployeeDAO();
-//        employeeDAO.createEmployeeTable();
-//        concurrently(validEntries, employeeDAO, 32);
-//        long end = System.nanoTime();
-//
-//        int duplicatedRecords = CSVReader.getDuplicatedEntries().size();
-//        int corruptedRecords = CSVReader.getCorruptedEntries().size() + duplicatedRecords;
-//        DisplayManager.printPersistingResults(start, end, validEntries.size(), corruptedRecords, duplicatedRecords,0);
-//        return employeeDAO;
-//    }
 }
